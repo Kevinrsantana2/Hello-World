52d1aa707bb6c556fe893b5e5a7c56ffa1b918cf
Kevin Ruiz Santana <124311914+Kevinrsantana2@users.noreply.github.com>
2023-02-20 16:59:35 -0800

    Hello World code entered

---
+++ helloWorld.py
@@ -0,0 +1,12 @@
+"""
+    This is the hello world program in python
+"""
+import sys
+
+# print the version of python you are running, it should be 3.x
+print()
+print("Version is: ", sys.version)
+print()
+
+# print hello world
+print("Hello World")
